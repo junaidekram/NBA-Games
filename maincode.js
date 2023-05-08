581d22a32d020655f0dddcaad62dfb3fb3ee4979
Fiercestar <112276644+junaidekram@users.noreply.github.com>
2023-05-08 10:50:18 -0500

    made outline

--- maincode.js
+++ maincode.js
@@ -1,1 +1,25 @@
+const canvas = document.getElementById("game-area");
+const c = canvas.getContext("2d");
+//const guestT = document.getElementById("guest");
+//const homeT = document.getElementById("home1");
+var gscore;
+var hscore;
+function generateScores(){
+    alert("work!")
+}
+function ShowResults(){
+    const guestT = document.getElementById("guest");
+    const homeT = document.getElementById("home1");
+    const canvas = document.getElementById("game-area");
+    const c = canvas.getContext("2d");
+    var hometeam1 = homeT.value;
+    var awayteam1 = guestT.value;
+    if(hometeam1 == awayteam1){
+        c.fillStyle = "green";
+        c.fillText("Cannot have the same team as home and guest team", 0, 200)
+    }else{
 
+        console.log("ff")
+        generateScores();
+    }
+}
